73c4f62d71331454cca099b09043c6185be1cb67
Alvaro Farina <farinalvaro@gmail.com>
2019-11-28 17:10:15 -0300

    Updates

---
+++ precache-manifest.8632acb4dabd17081e8f20082c3894fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0123eda4c8188517e6237af5d62ac65f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "62f95e5d31957ac98f35",
+    "url": "/monsters-rolodex/static/css/main.7f24a142.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "62f95e5d31957ac98f35",
+    "url": "/monsters-rolodex/static/js/main.f6469a3a.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
